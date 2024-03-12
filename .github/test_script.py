2b8091f4ef341cd0243d125fe087099af9d62e8f
Your NameMoustafa-Naja <moustafanaja@gmail.com>
2024-03-12 17:18:24 +0100

    push dataset

--- .github/test_script.py
+++ .github/test_script.py
@@ -2,3 +2,7 @@ with open('.github/workflows/creditmodel.sav', 'rb') as f:
     pickeled_model = pickle.load(f)
 
 import numpy as np
+
+# Load the test data
+X_test = np.load('.github/workflows/final_df_5.csv')
+y_test = np.load('.github/workflows/final_df_5.csv')
