e7b704cbeb1cb8afddebdd35f966427b50136a5c
Your NameMoustafa-Naja <moustafanaja@gmail.com>
2024-03-12 17:42:18 +0100

    update

--- .github/test_script.py
+++ .github/test_script.py
@@ -1,8 +1,24 @@
-with open('.github/workflows/creditmodel.sav', 'rb') as f:
+import pickle
+import pandas as pd
+
+with open('C:/Users/m.naja/P7/.github/workflows/creditmodel.sav', 'rb') as f:
     pickeled_model = pickle.load(f)
 
 import numpy as np
 
+
+df = pd.read_csv('C:/Users/m.naja/P7/.github/workflows/final_df_5.csv')
+
 # Load the test data
-X_test = np.load('.github/workflows/final_df_5.csv')
-y_test = np.load('.github/workflows/final_df_5.csv')
+X_test = df["EXT_SOURCE_2", "EXT_SOURCE_3", "CNT_FAM_MEMBERS", "DAYS_REGISTRATION", "AMT_REQ_CREDIT_BUREAU_HOUR"]
+y_test = df['TARGET']
+
+# Make predictions
+predictions = pickeled_model.predict(X_test)
+
+# Evaluate the model
+accuracy = np.mean(predictions == y_test)
+print(f"Accuracy: {accuracy}")
+
+# Assert the model performance is as expected
+assert accuracy > 0.8  
