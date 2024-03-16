26af33c6bcd423a2761793b579e4205e403fc8d4
Moustafa Naja <90932567+Moustafa-Naja@users.noreply.github.com>
2024-03-16 12:49:22 +0100

    Update test_script.py

--- .github/project_files/test_script.py
+++ .github/project_files/test_script.py
@@ -1,13 +1,13 @@
 import pickle
 import pandas as pd
 
-with open('C:/Users/m.naja/P7/.github/workflows/creditmodel.sav', 'rb') as f:
+with open('creditmodel.sav', 'rb') as f:
     pickeled_model = pickle.load(f)
 
 import numpy as np
 
 
-df = pd.read_csv('C:/Users/m.naja/P7/.github/workflows/final_df_5.csv')
+df = pd.read_csv('final_df_5.csv')
 
 # Load the test data
 X_test = df["EXT_SOURCE_2", "EXT_SOURCE_3", "CNT_FAM_MEMBERS", "DAYS_REGISTRATION", "AMT_REQ_CREDIT_BUREAU_HOUR"]
